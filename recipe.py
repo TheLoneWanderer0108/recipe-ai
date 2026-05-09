444f525df95f9c105a162e2e323087f30db2b5b0
Uziel Almonte <uxar0001@ce.pucmm.edu.do>
2026-05-09 13:14:12 -0400

    Added tools to the ai for ingredient substitute, will add more advanced substitute tool, e.g ai within a tool

--- recipe.py
+++ recipe.py
@@ -1,7 +1,27 @@
 import os
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
+from langchain.tools import tool
+
+@tool
+def ingredient_subs(ingredient: str) -> str:
+    """Only return a substitute for the ingredient provided. 
+    Do not return any explanation or description. Just the substitute."""
+
+    substitutes = {
+        "eggs": "1/4 cup unsweetened applesauce or 1/4 cup mashed banana",
+        "milk": "unsweetened almond milk or soy milk",
+        "butter": "coconut oil or olive oil",
+        "sugar": "honey or maple syrup",
+        "flour": "almond flour or oat flour",
+        "baking powder": "1/4 teaspoon baking soda + 1/2 teaspoon vinegar or lemon juice",
+        "vanilla extract": "1/2 teaspoon almond extract or 1/2 teaspoon maple syrup",
+        "salt": "1/4 teaspoon sea salt or 1/4 teaspoon kosher salt"
+    }
+    return substitutes.get(ingredient.lower(), "No substitute found for this ingredient.")
+    
+
 
 load_dotenv()
 #choose model here
@@ -9,6 +29,8 @@ llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash-lite",
     temperature=0.7
 )
+
+llm_with_tools = llm.bind_tools([ingredient_subs])
 area = input("Enter the area of the world you want a recipe from: ")
 # this is the prompt template, it will be used to generate the recipe based on the area that user inputted
 recipe_template = """You are a culinary expert. 
@@ -64,3 +86,13 @@ instructions = instructions_chain.invoke({"recipe": recipe, "ingredients": ingre
 print(f"Recipe: {recipe}")
 print(f"Ingredients: {ingredients}")
 print(f"Instructions: {instructions}")
+
+missing_ingredient = input("Enter an ingredient you want to substitute: ")
+response = llm_with_tools.invoke(f"What is a substitute for {missing_ingredient}?")
+if response.tool_calls:
+    tool_call = response.tool_calls[0]
+    ingredient = tool_call["args"]["ingredient"]
+    result = ingredient_subs.invoke(ingredient)
+    print(f"Substitute for {ingredient}: {result}")
+else:    print(response.content)
+
